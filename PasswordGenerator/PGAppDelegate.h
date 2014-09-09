09da39ff863b66889ce58ba45b3bfcb68719386e
James Shiell <james@infernus.org>
2014-09-09 12:05:50 +0100

    Initial Commit

---
+++ PasswordGenerator/PGAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  PGAppDelegate.h
+//  PasswordGenerator
+//
+//  Created by James Shiell on 09/09/2014.
+//  Copyright (c) 2014 Simia Infinitus. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PGAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ PasswordGenerator/PGViewController.m
@@ -0,0 +1,29 @@
+//
+//  PGViewController.m
+//  PasswordGenerator
+//
+//  Created by James Shiell on 09/09/2014.
+//  Copyright (c) 2014 Simia Infinitus. All rights reserved.
+//
+
+#import "PGViewController.h"
+
+@interface PGViewController ()
+
+@end
+
+@implementation PGViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
